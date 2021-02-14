4762e7aba0e4ced327b11ee456f43a9e47e9d7e1
Garrett Brown <garrettpaulbrown@gmail.com>
2021-02-13 20:57:09 -0800

    Clean up JS code.

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,10 @@
 function rangeCreate(number) {
 	let array = [];
     for (let index = 0; index <= number; index += 1) {
-        array.push(index);
+        array.push((" ") + index);
     }
 	return array;
-}
-
-// function beepBoop(array) {
-// 	console.log(array.slice(1))
-// }
-
-// function beepBoop(array) {
-// 	array.splice(2, 0, "Beep!")
-//   console.log(array)
-// }
-
-
-// let array = [1,2,3];
-// beepBoop(array);
+};
 
 
 
@@ -29,10 +16,9 @@ $(document).ready(function() {
     $("form#neighborhood").submit(function(event) {
         event.preventDefault();
         let number = $("input#range").val();
-        console.log(typeof number)
-        // const fullArray = rangeCreate(number);
-        // console.log(fullArray);
+        let fullArray = rangeCreate(number);
 
-        // $("#print").text(fullArray);
+
+        $("#print").text(fullArray);
     })
 })
